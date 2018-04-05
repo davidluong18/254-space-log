0e4034dfdc12bca1341441b107c646f9f66ed87a
Student Person <upalpatel@csu.fullerton.edu>
2018-04-04 21:47:38 -0700

    planets file made

---
+++ planets.py
@@ -0,0 +1,11 @@
+import re
+
+def find_planet_names(content:str):
+	pattern = re.compile("\"BodyName\":\"[-\w+\s\d+]+")
+	planetResult = pattern.findall(content)
+	if planetResult:
+		for i in planetResult:
+			if i[0] == i[-1]:
+				i += 1
+			print(i+'"')
+	return len(planetResult)
