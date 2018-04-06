230ddf1e0bd0afdccb09d82b3c898b641f51d3ad
Student Person <upalpatel@csu.fullerton.edu>
2018-04-05 23:39:25 -0700

    using return not print

--- planets.py
+++ planets.py
@@ -4,9 +4,5 @@ import re
 def find_planet_names(content:str):
 	pattern = re.compile("\"BodyName\":\"[-\w+\s\d+]+")
 	planetResult = pattern.findall(content)
-	if planetResult:
-		for i in planetResult:
-			if i[0] == i[-1]:
-				i += 1
-			print(i+'"')
-	return len(planetResult)
+	return planetResult
+
