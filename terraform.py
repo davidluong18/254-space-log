14f71cbe637971fefb5be54cc5814f38f2954733
LeftyRoy <37429621+LeftyRoy@users.noreply.github.com>
2018-04-08 20:55:31 -0700

    Update terraform.py
    Now it should work woops.

--- terraform.py
+++ terraform.py
@@ -2,6 +2,6 @@
 import re
 
 def amount_terraformable_planets(content:str):
-	pattern = re.compile("\"Terraformable\"[-\w+\s\d+]+")
+	pattern = re.compile("\"Terraformable\"")
 	planetResult = pattern.findall(content)
 	return len(planetResult)
