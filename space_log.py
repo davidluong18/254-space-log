9a80d6e4a05e9f36e2119a48ee1901b580ceae38
davidluong18 <31758412+davidluong18@users.noreply.github.com>
2018-04-09 17:48:40 -0700

    Update space_log.py

--- space_log.py
+++ space_log.py
@@ -3,7 +3,7 @@
 # Use like `./space_log.py -s|-p|-t|-d|-f log_file
 
 from sys import argv
-import fuel
+import fuel, re, systems
 
 # Opens the log file and grabs the contents.
 try:
@@ -17,11 +17,11 @@ except:
 
 # Uncomment, and add your work in the appropriate spots.
 argSwitcher = {
-#	'-s': NAMES OF SYSTEMS VISITED
+	'-s': systems.find_system_names, # NAMES OF SYSTEMS VISITED
 #	'-p': NAMES OF PLANETS SCANNED
 #	'-t': TOTAL NUMBER OF TERRAFORMABLE PLANETS SCANNED
 #	'-d': TOTAL DISTANCE IN LIGHT YEARS
-	'-f': fuel.get_total_fuel,	# The example.
+#	'-f': fuel.get_total_fuel,	# The example.
 }
 
 try:
